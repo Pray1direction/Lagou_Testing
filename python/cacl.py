de82461dc9ed53510e200f98f501e6af0247ebc5
Pray <1807594337@qq.com>
2020-04-26 18:14:31 +0800

    第一次学习

---
+++ python/cacl.py
@@ -0,0 +1,7 @@
+class Calc:
+    #类型提示
+    def add(self, a: int, b: int) -> int:
+        return a + b
+
+    def div(self, a: float, b: float) -> float:
+        return a / b
---
+++ testing/test_pytest.py
@@ -0,0 +1,10 @@
+from python.cacl import Calc
+
+
+class TestCalc():
+    def setup(self):
+        self.calc = Calc()
+    def test_add(self):
+        assert self.calc.add(1, 2) == 3
+    def test_div(self):
+        assert self.calc.div(1,2) == 0.5
